17383db035299bead87bd75560f5cabeb518cb13
Brendo França <brendosbf@gmail.com>
2024-08-26 12:11:30 +0200

    Add project files.

---
+++ plus.minus/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace plus.minus
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PlusMinus([1, 1, 0, -1, -1]);
+        }
+
+        private static void PlusMinus(List<int> numbers)
+        {
+            var size = numbers.Count;
+
+            var totalPositiveNumbers = (decimal)numbers.Count(x => x > 0);
+
+            var totalNegativeNumbers = (decimal)numbers.Count(x => x < 0);
+
+            var totalZeroNumbers = (decimal)numbers.Count(x => x == 0);
+
+            Console.WriteLine((totalPositiveNumbers / size).ToString("F6"));
+
+            Console.WriteLine((totalNegativeNumbers / size).ToString("F6"));
+
+            Console.WriteLine((totalZeroNumbers / size).ToString("F6"));
+        }
+    }
+}
